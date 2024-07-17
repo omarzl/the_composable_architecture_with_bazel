260e672e2841bd4f2b1ca977982e6aeca8f620cb
omarzl <omarzl@hotmail.es>
2024-07-17 10:20:34 -0600

    deleted platform from package

--- Package.swift
+++ Package.swift
@@ -4,9 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "Demo",
-    platforms: [
-        .iOS(.v16),
-    ],
     dependencies: [
         .package(
             url: "https://github.com/pointfreeco/swift-composable-architecture.git",
